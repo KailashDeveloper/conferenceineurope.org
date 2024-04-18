0e68eae561a21fc278600132a16c81fabba8c8fe
sanjeeb <sanjeeb.dakhinaray@gmail.com>
2024-04-18 23:20:36 +0530

    - files

--- conferenceineurope.org/.control-OLD/upload.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$name = time().$_FILES["images"]["name"];
-$_SESSION['ad_image']=$name;        
-        move_uploaded_file( $_FILES["images"]["tmp_name"], "../ad/" . $name);
-   
-echo "Successfully Uploaded Images";
-?>
