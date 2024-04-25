405df0937dd51f363de03dcfc54595ae504d27d8
AsishITR <asish.itresearch@gmail.com>
2024-04-25 17:44:45 +0530

    test3

--- conferenceineurope.org/place_menu.php
+++ conferenceineurope.org/place_menu.php
@@ -10,7 +10,7 @@
 
    <h1><?php if (isset($meta_title) and $meta_title == 1) echo $meta_title;
         else { ?>
-
+<?php echo $meta_title ?>
        Conferences in <?php echo $place; ?> <?php echo date("Y"); ?> <?php } ?> </h1>
 
 
@@ -21,11 +21,11 @@
     else { ?>
 
      <p>
-     Search and find all international conferences in <?php echo $place; ?> for latest discussion and networking with academic, professional and industry experts. conferenceineurope brings you complete details or current and upcoming conferences across academic fields of study. This site is leading with the best conference listing and promotion platform for scholars, students, academicians, professionals and experts. Here, you can find many verified international conferences in <?php echo $place; ?> on different subjects like Medical Conference, Engineering Conference, Mathematics Conference, Management Conference, Business Conference and Social Science Conference etc.
+       Search and find all international conferences in <?php echo $place; ?> for latest discussion and networking with academic, professional and industry experts. conferenceineurope brings you complete details or current and upcoming conferences across academic fields of study. This site is leading with the best conference listing and promotion platform for scholars, students, academicians, professionals and experts. Here, you can find many verified international conferences in <?php echo $place; ?> on different subjects like Medical Conference, Engineering Conference, Mathematics Conference, Management Conference, Business Conference and Social Science Conference etc.
      </p>
 
      <p>
-     As a researcher, you can explore many more knowledge and a global platform through international conference in <?php echo $place; ?> with participants and speak with experts. We are trusted by thousands of professionals, academicians and conference organizers to post, index and promote their conferences worldwide. So, to know more about all the upcoming international conferences in <?php echo $place; ?>  <?php echo date("Y"); ?>, search and subscribe to conferenceineurope.
+       As a researcher, you can explore many more knowledge and a global platform through international conference in <?php echo $place; ?> with participants and speak with experts. We are trusted by thousands of professionals, academicians and conference organizers to post, index and promote their conferences worldwide. So, to know more about all the upcoming international conferences in <?php echo $place; ?> <?php echo date("Y"); ?>, search and subscribe to conferenceineurope.
      </p>
 
      <br>
